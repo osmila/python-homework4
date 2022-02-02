9a5cbe0cdc9d9b66e14986c68d513679649ea3e7
oksana.smila <oksana.smila@exness.com>
2022-02-02 23:22:03 +0200

    Task 1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello, world!')
+import task1_convert_list_to_tuple
+
+# Task 1:
+# Напишите программу для преобразования списка в кортеж
+
+print('Task 1: Convert list to tuple:')
+task1_convert_list_to_tuple.convert_list_to_tuple()
