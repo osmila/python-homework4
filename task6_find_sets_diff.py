a70b7d93f41ccec9a1583f7446079d8368d9afa4
oksana.smila <oksana.smila@exness.com>
2022-02-05 21:51:51 +0200

    Task 7

--- task6_find_sets_diff.py
+++ task6_find_sets_diff.py
@@ -12,3 +12,16 @@ def find_sets_diff():
         print('There are NO elements in set A that are not presented in set B')
     else:
         print(f'Difference between set A and set B: {diff_set}')
+
+
+def delete_diff_set():
+    set_A = generate_random_data.create_set(string_add=False)
+    set_B = generate_random_data.create_set(string_add=False)
+    print(f'Test sets are:\nA: {set_A}\nB: {set_B}')
+
+    set_A = set_A - set_B
+
+    if len(set_A) == 0:
+        print('There are NO elements left in set A')
+    else:
+        print(f'Set A after deleting set B from it: {set_A}')
