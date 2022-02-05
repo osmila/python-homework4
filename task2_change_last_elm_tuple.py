78fc8d8bebf8a93e932ad904f90362a50ce094e2
oksana.smila <oksana.smila@exness.com>
2022-02-05 16:33:36 +0200

    Task 3

--- task2_change_last_elm_tuple.py
+++ task2_change_last_elm_tuple.py
@@ -1,19 +1,8 @@
-import task1_convert_list_to_tuple
-import random
-
-
-def create_list_of_tuples(tuples_max_count=5):
-    tuples_count = random.randrange(1, tuples_max_count)
-    list_tuples = list()
-    for i in range(tuples_count):
-        temp_list = task1_convert_list_to_tuple.generate_list_random(string_exist=True,
-                                                                     list_min_length=2, list_max_length=6)
-        list_tuples.append(tuple(temp_list))
-    return list_tuples
+import generate_random_data
 
 
 def change_last_tuple_element_in_list(changed_value):
-    list_to_update = create_list_of_tuples()
+    list_to_update = generate_random_data.create_list_of_tuples()
     result_list = list()
     print(f'Initial list:\t{list_to_update}')
     for i in range(len(list_to_update)):
